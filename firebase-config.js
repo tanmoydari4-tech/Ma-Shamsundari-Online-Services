dcacdda579338e0b03f971de98aaf6d6d56bbbe0
tanmoydari4-tech <tanmoydari4@gmail.com>
2026-08-29 10:49:44 +0530

    Add files via upload

---
+++ firebase-config.js
@@ -0,0 +1,10 @@
+// Ma Shamsundari Online Services - Firebase Web App configuration
+export const firebaseConfig = {
+  apiKey: "AIzaSyD6UixKB7z959Q1Bc77Hkw1A4juAQZ8HOc",
+  authDomain: "ma-shamsundari-online-services.firebaseapp.com",
+  projectId: "ma-shamsundari-online-services",
+  storageBucket: "ma-shamsundari-online-services.firebasestorage.app",
+  messagingSenderId: "105298116935",
+  appId: "1:105298116935:web:8f9c513abc009f242c0c62",
+  measurementId: "G-MB8635YYGQ"
+};
